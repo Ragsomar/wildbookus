ba9f001dd410f838f29208b348bd1b988c3863e2
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-29 15:49:43 +0100

    ADD all files from before

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import WilderCard from './components/WilderCard'
+import './style/Image.css'
+import './style/Layout.css'
+import './style/Border.css'
+import './style/Space.css'
+import './style/Color.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className=''>
+			<WilderCard />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ front/src/components/WilderCard.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const wilder = {
+	name: 'Pierre-Alban',
+	picture: 'https://zupimages.net/up/20/05/nv4m.jpg'
+}
+
+class WilderCard extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<div className='flex-column'>
+				<div className='flex-row space:inset-squish space:stack flex-both:center'>
+					<div className='flex-column full-bord space:inline flex:50/50 round-bord profile-card-bg-color'>
+						<img
+							src={wilder.picture}
+							alt='PA'
+							className='profile-pic-size round-pic space:inset-squish'
+						/>
+						<h3 className='space:inset-stretch space:stack space-size:s space:inset-squish'>
+							{wilder.name}
+						</h3>
+					</div>
+					<div className='flex-column full-bord space:inline flex:50/50 round-bord'>
+						<img
+							src={wilder.picture}
+							alt='PA'
+							className='profile-pic-size round-pic space:inset-squish'
+						/>
+						<h3 className='space:inset-stretch space:stack  space-size:s space:inset-squish'>
+							{wilder.name}
+						</h3>
+					</div>
+				</div>
+				<div className='flex-row  space:inset-squish space:stack flex-both:center'>
+					<div className='flex-column full-bord space:inline flex:50/50 round-bord'>
+						<img
+							src={wilder.picture}
+							alt='PA'
+							className='profile-pic-size round-pic space:inset-squish'
+						/>
+						<h3 className='space:inset-stretch space:stack space-size:s space:inset-squish'>
+							{wilder.name}
+						</h3>
+					</div>
+					<div className='flex-column full-bord space:inline flex:50/50 round-bord'>
+						<img
+							src={wilder.picture}
+							alt='PA'
+							className='profile-pic-size round-pic space:inset-squish'
+						/>
+						<h3 className='space:inset-stretch space:stack  space-size:s space:inset-squish'>
+							{wilder.name}
+						</h3>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default WilderCard
