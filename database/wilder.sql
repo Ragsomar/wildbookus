8c03443bd47c41346c3909c074fa15718b001b64
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-30 11:18:21 +0100

    ADD script to add datas in database

---
+++ database/wilder.sql
@@ -0,0 +1,120 @@
+USE wildbookus;
+
+INSERT INTO
+    wilder (id, name, picture)
+VALUES
+    (
+        1,
+        "Maxime Féry",
+        "https://media-exp1.licdn.com/dms/image/C4E03AQFgnfYD8ycoPA/profile-displayphoto-shrink_200_200/0?e=1585785600&v=beta&t=pXa9rwftmrWb7wZM1CknrUVEN0z18qJ_1hV-nzq_d_w"
+    ),
+    (
+        2,
+        "Xénia Lecomte",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHslrnslL8tlw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=tW1D_jamRM18ROzA3P7AAdJ76RyTSLjB6A3Ern6Cm_U"
+    ),
+    (
+        3,
+        "Hervé Bourelle",
+        "https://media-exp1.licdn.com/dms/image/C4E03AQHzaRK9Ep_dxg/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=b3w92IjSjCwn0xqdLnrT3veP3YPKJS3G_zgXQ_iX4SY"
+    ),
+    (
+        4,
+        "Sylvie Lecrocq",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQFY7FnZJEEF6w/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=Wbz26R2icsZcirOdkl83sDVGPJaBXLaN4JNty92J8PM"
+    ),
+    (
+        5,
+        "Pascal Jurion",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHFfpqbAi-j_Q/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=YD1YaldK1yW5nOlAioUGMaS6XZGgb1gDjfWrH3o1lXg"
+    ),
+    (
+        6,
+        "Hugo Vallée",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHrrDL9R2VmVw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=o6wqyRLQZBbEIUK__5E_FjKcT75p_A7DnEjPZumLIq0"
+    ),
+    (
+        7,
+        "Christopher Poncelet",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHRBnCQQo1Fyw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=L7Wbiov-S82U8q5dYRDaG3Dep6DuvWQKDlwJoe5tNuI"
+    ),
+    (
+        8,
+        "Manaf Sultan",
+        "https://media-exp1.licdn.com/dms/image/C5603AQF_VRL7uPqQ0A/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=ZYtBgUioJSsQXER8FWfr-trH0EWZn5M1HINy9Szr7SE"
+    ),
+    (
+        9,
+        "Alexis Tonello",
+        "https://media-exp1.licdn.com/dms/image/C5603AQEVJ4FkUhQilw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=Ogvoma9qtpF8QoIy95K1bBpaEhKrcdnrubUXsFtqfns"
+    ),
+    (
+        10,
+        "Sophie Trochain",
+        "https://media-exp1.licdn.com/dms/image/C5603AQF5e10ILgstNw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=_5gaiMHV71sKbIKr0CS8kwXyQhbPH5ou1CpGx8g18jo"
+    ),
+    (
+        11,
+        "Jessy Pinchon",
+        "https://media-exp1.licdn.com/dms/image/C4E03AQEdfKTVsGNxCQ/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=hyhFFL5B_2abkOOjiJOoZDA03odnFIB9Fb9g4VIZn2A"
+    ),
+    (
+        12,
+        "Marine Robert",
+        "https://media-exp1.licdn.com/dms/image/C5603AQH7-VjD_s_qOg/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=6I1yMa12YrXu-d81PWUoNKn4ag7rNnLrc6wO7Ct7zt0"
+    ),
+    (
+        13,
+        "Wojtek Zdebski",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQH6XTIdXkXIhA/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=VX_wi8GPo7FZeAlDAJgwlAq6i0ROPANiopAx9mf32UU"
+    ),
+    (
+        14,
+        "Maxime Thibaud",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQFgjUKKwn_GTQ/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=jP-6XJKL86fHLAebFcHF2WntanLQ_MZ0OqqwMDq7A8A"
+    ),
+    (
+        15,
+        "Cindie Jouvin",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQFxVa9ExlfbnQ/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=HXx1g8y-imATThsl-HktnTD_qIg0Pl17pL1aD3SWy4g"
+    ),
+    (
+        16,
+        "Victor Biard",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHajb7PNMkTGQ/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=5jwNIXBB1sfD7tJOj78pmeqznt7db864oMwZ98sx98U"
+    ),
+    (
+        17,
+        "Jessy Corabeouf",
+        "https://media-exp1.licdn.com/dms/image/C4E03AQH-hffg25kuKw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=YcgggFXsRdRU7w1bgy_ZTlPMvdRtAdVKdo1MKy3n5QI"
+    ),
+    (
+        18,
+        "Anthony De Sousa",
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHpbz3vaj94tw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=HoJWhcuBfC-Ok200CLqiL3hw2a8qnwBYaUFFlmweQMU"
+    ),
+    (
+        19,
+        "Pierre-Alban Demars",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHqM-i0avXyWw/profile-displayphoto-shrink_200_200/0?e=1585785600&v=beta&t=7vHKt7dvdJH_bRsYuWVTy7K6unw9e8-X7ZXeL2KzORA"
+    ),
+    (
+        20,
+        "Pablo Martin-Onetti",
+        "https://media-exp1.licdn.com/dms/image/C5603AQGkgaMYLAa_AA/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=jjREmp446ud0D4DrLBXC0pfC30Aj-nXOupW5ferM60Q"
+    ),
+    (
+        21,
+        "Pierre Moulin",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHEMjazxz0kwA/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=beqVflAAU1fdiaSh2sKbB2ozcBFXvf_rgCWkmJXwPjs"
+    ),
+    (
+        22,
+        "Valentin Julien",
+        "https://media-exp1.licdn.com/dms/image/C4E03AQG-WYE2F3pm4g/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=mU69oBZfvYYB-1DwnSAYoecHynVo_fsIJBXFthbKhzo"
+    ),
+    (
+        23,
+        "François Hanesse",
+        "https://media-exp1.licdn.com/dms/image/C5603AQGW_EE58BSITw/profile-displayphoto-shrink_800_800/0?e=1585785600&v=beta&t=69cGb-OH12h6pZgdrwFOGi8pb2wfzsjIKnHmjQLvrvI"
+    );
